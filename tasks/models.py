24a3b9e6a82bbbb0097fa8bff1be90efb876e52f
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-02-02 00:15:50 +0330

    task model created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class Task(models.Model):
+    creator = models.ForeignKey( User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
