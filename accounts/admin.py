e4d8902a628ee9ffbcf0a1efb5e47a34111003ed
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-02-06 00:29:37 +0330

    user admin and task admin created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model =User
+    list_display =('email' ,)
+    list_filter =('is_active' , 'is_staff' ,)
+    search_fields = ('email' ,)
+    ordering = ('email' ,)
+    fieldsets = (
+        ("Authenticate", {"fields": ("email", )}),
+         ("premition", {"fields": ("is_active", "is_superuser","is_staff")}),
+    
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active" )}
+        ),
+    )
+    
+    
+
+admin.site.register(User , CustomUserAdmin)    
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskCustomAdmin(admin.ModelAdmin):
+    model = Task
+    list_display = ('title' , 'status' , 'updated_date' , 'created_date')
+    list_filter =('status' , 'created_date')
+    
+    
+    
+admin.site.register(Task , TaskCustomAdmin)    
+
