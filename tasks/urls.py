7d5b888c4fd5a748ef4852dba9b89f8bc8af7057
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-02-06 00:52:17 +0330

    task display created

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexViews
+from .views import IndexViews , TaskViews
 
 app_name = 'tasks'
 
 urlpatterns = [
-    path('' , IndexViews.as_view() , name= 'index' ),
+   # path('' , IndexViews.as_view() , name= 'index' ),
+    path('' , TaskViews.as_view() , name= 'index' ),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView , ListView
+from .models import Task
 
 class IndexViews(TemplateView):
     template_name ='index.html'
+    
+    def get_context_data(self, **kwargs):
+        
+        return super().get_context_data(**kwargs)
+
+class TaskViews(ListView):
+    context_object_name = 'tasks'
+    template_name = 'index.html'
+    def get_queryset(self):
+        tasks = Task.objects.all()
+        return tasks
