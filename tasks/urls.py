258dbedb6a65b955d09c2ef4f394e57e8e9caded
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-02-06 01:36:29 +0330

    add button added

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import IndexViews , TaskViews
+from .views import  TaskViews , CreateTaskViews
 
 app_name = 'tasks'
 
 urlpatterns = [
    # path('' , IndexViews.as_view() , name= 'index' ),
     path('' , TaskViews.as_view() , name= 'index' ),
+    path('add/' , CreateTaskViews.as_view() , name='add'), 
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView , ListView
+from django.shortcuts import render , redirect
+from django.views.generic import TemplateView , ListView , CreateView
 from .models import Task
-
-class IndexViews(TemplateView):
-    template_name ='index.html'
-    
-    def get_context_data(self, **kwargs):
-        
-        return super().get_context_data(**kwargs)
+from django.urls import reverse_lazy 
 
 class TaskViews(ListView):
     context_object_name = 'tasks'
@@ -15,3 +9,22 @@ class TaskViews(ListView):
     def get_queryset(self):
         tasks = Task.objects.all()
         return tasks
+    
+class CreateTaskViews(CreateView):
+    model = Task
+    fields = ['title']
+    success_url = reverse_lazy('tasks:index')
+    template_name = 'index.html'
+    
+    def form_valid(self, form):
+        form.instance.creator =self.request.user
+        return super().form_valid(form)
+def add_task(request):
+    if request.method == "POST":
+        print("POST request received")
+        print(request.POST)  # Debugging input data
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
+            print("Task saved!")  # Confirm saving
+        return redirect("tasks:add")
