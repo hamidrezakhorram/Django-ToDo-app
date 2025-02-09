fbb09abe0389914ef5a1a4ffcc777b67c1aefb6a
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-02-09 13:06:24 +0330

    login view added

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 class UserLoginViews(LoginView):
     template_name = 'accounts/index.html'
     success_url =reverse_lazy('tasks:index')
-    # def get_success_url(self):
-    #     return self.success_url
+    def get_success_url(self):
+        return self.success_url
+   
      
 
 
